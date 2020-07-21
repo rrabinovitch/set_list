a69e1dc6e47df6a24276b829ea594b82b95834ee
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-07-20 18:50:02 -0600

    07.16.20 authentication lesson

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    user = User.find_by(username: params[:username])
+
+    if user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome, #{user.username}"
+      redirect_to '/'
+    else
+      flash[:error] = "Sorry, your credentials are bad."
+      redirect_to "/login"
+    end
+  end
+end
